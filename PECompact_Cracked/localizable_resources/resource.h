5ce1a76ba91c4d4ec83d018c7fe7cb9b50ca6210
orlando <75084509+dehoisted@users.noreply.github.com>
2021-08-21 23:51:57 -0400

    Update resource.h

--- PECompact_Cracked/localizable_resources/resource.h
+++ PECompact_Cracked/localizable_resources/resource.h
@@ -274,14 +274,3 @@
 #define IDM_BATCHFILE                   40014
 #define ID_ACTION_BUILDBATCHFILEFORSELECTEDITEMS 40015
 #define ID_PROJECT_EXPORTPROJECTTOBATCHFILE 40016
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        149
-#define _APS_NEXT_COMMAND_VALUE         40017
-#define _APS_NEXT_CONTROL_VALUE         1070
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
